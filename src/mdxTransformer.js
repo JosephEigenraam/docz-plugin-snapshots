0c0adcbae2cf088294b0f51af54af68adcf924b9
Joseph Black <josephconradblack@gmail.com>
2018-08-14 12:50:43 +1000

    Remove incorrect comment

--- src/mdxTransformer.js
+++ src/mdxTransformer.js
@@ -2,7 +2,7 @@ const createMdxTransformer = require("jest-mdx-loader/src/createTransformer");
 
 const preProcess = function(src) {
   return src
-    .replace(/<Playground>/g, "<React.Fragment>") // Auto-convert tags to div
+    .replace(/<Playground>/g, "<React.Fragment>")
     .replace(/<\/Playground>/g, "</React.Fragment>");
 };
 
